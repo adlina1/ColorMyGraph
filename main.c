65f5cb3e84c2f57563c76f5fe1affb37bba0cdc3
adlina1 <adrien28210@gmail.com>
2022-11-12 17:50:49 +0100

    implemented a method to get aware of neighbor's processes : the adjacency matrix

--- main.c
+++ main.c
@@ -6,10 +6,9 @@
 #include <ctype.h>
 #include <assert.h>
 
-
-// #define NROW 15
 #define NCOL 2
 
+int N,M;
 
 // Will be used to remove the 'e'
 void removeChar(char* s, char c)
@@ -76,6 +75,40 @@ while ((read = getline(&line, &len, file)) != -1) {
 }
 
 
+// Adj: Cr_GFK
+void createAdjMatrix(int Adj[][N+1], int arr[][2]) {
+
+    // Intializing all values to 0
+    for (int i=0; i<N+1; i++) {
+
+        for (int j=0; j<N+1; j++) {
+            Adj[i][j] = 0;
+        }
+    }
+
+    // Creation of the adjacency matrix
+    for (int i=0; i<M; i++) {
+
+        int x = arr[i][0];
+        int y = arr[i][1];
+
+        Adj[x][y] = 1;
+        Adj[y][x] = 1;
+    }
+}
+
+
+void showAdjMatrix(int Adj[][N + 1])
+{
+
+    for (int i=1; i<N+1; i++) {
+        for (int j=1; j<N+1; j++) {
+            printf("%d", Adj[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 
 
 int main(int argc, char **argv) 
@@ -117,37 +150,52 @@ int main(int argc, char **argv)
 
     removeChar(result, 'e');
 
-  const char * separator = " ";
-  char * strToken;
-  char *outt;
-  char *end;
-  /* 1D array */
+    const char * separator = " ";
+    char * strToken;
+    char *outt;
+    char *end;
 
-  int nb_row = (getNbEdges/2);
+    /* 1D array */
+    int nb_row = (getNbEdges/2);
 
-  int arrBuf[nb_row*NCOL];
-  /* 2D array */
-  int (*myArr)[NCOL] = (int(*)[NCOL])arrBuf;
-  int i;
-  outt = strdup(result);
-  /* Store numbers as 1D array */
-  strToken = strtok ( outt, separator );
-  for (i = 0; i < nb_row*NCOL && strToken != NULL ; i++) {
+    int arrBuf[nb_row*NCOL];
+    /* 2D array */
+    int (*myArr)[NCOL] = (int(*)[NCOL])arrBuf;
+    int i;
+    outt = strdup(result);
+    /* Store numbers as 1D array */
+    strToken = strtok ( outt, separator );
+    for (i = 0; i < nb_row*NCOL && strToken != NULL ; i++) {
     arrBuf[i] = (int)strtol(strToken, &end, 10);
     strToken = strtok ( NULL, separator);
-  }
+    }
+  
   /* Display numbers as 2D array */
-  for (i = 0; i < nb_row; i++) {
-    printf("%d %d\n", myArr[i][0], myArr[i][1]);
-  }
+  // for (i = 0; i < nb_row; i++) {
+  //   printf("%d---%d\n", myArr[i][0], myArr[i][1]);
+  // }
 
+    /* Test - Check for a certain val */
   // printf("CHECK: %d\n", myArr[9][1]);
+    
+    N = nbNodes;
+    M = nb_row; // M = # Edges
 
-  free(outt);
-  free(result);
+    // Adjacency matrix
+    int Adj[N + 1][N + 1];
 
-  return 0;
+
+    createAdjMatrix(Adj, myArr);
+    showAdjMatrix(Adj);
+
+
+    free(outt);
+    free(result);
+
+    return 0;
 
 
 } // END MAIN
+
+
 
